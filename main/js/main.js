e3f2fdf3a22e66d46196b8de172134655eb354c9
Airrocket <airrocket.buksas@gmail.com>
2018-12-25 07:35:00 -0700

    animate dropdown arrows

--- main/js/main.js
+++ main/js/main.js
@@ -1,15 +1,47 @@
 $(document).ready(() => {
+  var rotation = 0;
+  var value = 0;
   $('.fakeimg').slideUp();
 
+  //anime roulette card//
   $('#arrow-1').on('click', () => {
     $('#animeRoulette').slideToggle();
+    if (rotation == 0) {
+      value = 90;
+      $('#arrow-1').rotate({animateTo:value});
+      rotation = 90;
+    } else if (rotation == 90) {
+      value = 0;
+      $('#arrow-1').rotate({animateTo:value});
+      rotation = 0;
+    }
   })
 
+  //wdfd card//
   $('#arrow-2').on('click', () => {
     $('#wdfd').slideToggle();
+    if (rotation == 0) {
+      value = 90;
+      $('#arrow-2').rotate({animateTo:value});
+      rotation = 90;
+    } else if (rotation == 90) {
+      value = 0;
+      $('#arrow-2').rotate({animateTo:value});
+      rotation = 0;
+    }
   })
 
+  //minesweeper card//
   $('#arrow-3').on('click', () => {
     $('#minesweeper').slideToggle();
+    if (rotation == 0) {
+      value = 90;
+      $('#arrow-3').rotate({animateTo:value});
+      rotation = 90;
+    } else if (rotation == 90) {
+      value = 0;
+      $('#arrow-3').rotate({animateTo:value});
+      rotation = 0;
+    }
   })
 });
